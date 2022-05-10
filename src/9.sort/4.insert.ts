fad3e4ff388a1c91405bfc1bfeeb1bcc5a41b605
chenzhi16 <chenzhi16@meituan.com>
2022-05-10 13:11:09 +0800

    update

--- src/9.sort/4.insert.ts
+++ src/9.sort/4.insert.ts
@@ -1,16 +1,24 @@
 /*
-* Author: Chen Zhi <chenzhibupt@qq.com>
-* Date: May 9, 2022
-*
-* Insert Sort
-* 
-* A lot like poker game, find place for poker card in sorted cards.
-*/
+ * Author: Chen Zhi <chenzhibupt@qq.com>
+ * Date: May 9, 2022
+ *
+ * Insert Sort
+ *
+ * A lot like poker game, find place for poker card in sorted cards.
+ */
 
 import { init, swap } from '../common/utils';
 
 const insertSort = (arr: number[]): void => {
-  
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = i - 1; j >= 0; j--) {
+      if (arr[j + 1] < arr[j]) {
+        swap(arr, j + 1, j);
+      } else {
+        break;
+      }
+    }
+  }
 };
 
 const arr = init();
---
+++ src/9.sort/9.quick.ts
@@ -0,0 +1,20 @@
+/*
+ * Author: Chen Zhi <chenzhibupt@qq.com>
+ * Date: May 9, 2022
+ *
+ * Quick Sort
+ *
+ *
+ *
+ */
+
+import { init, swap } from '../common/utils';
+
+const quickSort = (arr: number[]): void => {
+  
+};
+
+const arr = init();
+console.log(arr);
+quickSort(arr);
+console.log(arr);
