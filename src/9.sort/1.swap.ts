509edeae5f3c14276a2792896a66d3b86789ecac
chenzhi16 <chenzhi16@meituan.com>
2022-05-10 12:39:25 +0800

    update

--- src/9.sort/1.swap.ts
+++ src/9.sort/1.swap.ts
@@ -1,16 +1,22 @@
 /*
-* Author: Chen Zhi <chenzhibupt@qq.com>
-* Date: May 9, 2022
-*
-* Swap Sort
-* 
-* Swap mininum number to leftest.
-*/
+ * Author: Chen Zhi <chenzhibupt@qq.com>
+ * Date: May 9, 2022
+ *
+ * Swap Sort
+ *
+ * Swap mininum number to leftest.
+ */
 
 import { init, swap } from '../common/utils';
 
 const swapSort = (arr: number[]): void => {
-  
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        swap(arr, i, j);
+      }
+    }
+  }
 };
 
 const arr = init();
