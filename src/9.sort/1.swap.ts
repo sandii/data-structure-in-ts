208bd40113c31db0103386030a5fc20e1dbb65c5
chenzhi16 <chenzhi16@meituan.com>
2022-05-10 19:56:35 +0800

    update

--- src/9.sort/1.swap.ts
+++ src/9.sort/1.swap.ts
@@ -1,11 +1,11 @@
 /*
- * Author: Chen Zhi <chenzhibupt@qq.com>
- * Date: May 9, 2022
- *
- * Swap Sort
- *
- * Swap mininum number to leftest.
- */
+* Author: Chen Zhi <chenzhibupt@qq.com>
+* Date: May 9, 2022
+*
+* Swap Sort
+* 
+* Swap mininum number to leftest.
+*/
 
 import { init, swap } from '../common/utils';
 
--- src/9.sort/9.quick.ts
+++ src/9.sort/9.quick.ts
@@ -3,10 +3,10 @@
  * Date: May 9, 2022
  *
  * Quick Sort
- * 
+ *
  * - Find array pivot, put smaller values to its left and bigger values to its right
  * - Process left and right arrays recursively
- * 
+ *
  * - In practice, we firstly put pivot to leftest
  * - Traverse array, make smallers left and biggers right
  * - Swap pivot and rightest of smallers
@@ -14,11 +14,25 @@
 
 import { init, swap } from '../common/utils';
 
-const quickSort = (arr: number[]): void => {
-
+const quickSort = (arr: number[], low: number, high: number): void => {
+  if (low >= high) return;
+  let pivotKey = Math.floor((low + high) / 2);
+  const pivotValue = arr[pivotKey];
+  swap(arr, low, pivotKey);
+  let writer = low;
+  for (let i = writer + 1; i <= high; i++) {
+    if (arr[i] < pivotValue) {
+      writer++;
+      swap(arr, i, writer);
+    }
+  }
+  swap(arr, low, writer);
+  pivotKey = writer;
+  quickSort(arr, low, pivotKey - 1);
+  quickSort(arr, pivotKey + 1, high);
 };
 
 const arr = init();
 console.log(arr);
-quickSort(arr);
+quickSort(arr, 0, arr.length - 1);
 console.log(arr);
