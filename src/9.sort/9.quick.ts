df91c60c0179af0720a863199e819b163540bd39
chenzhi16 <chenzhi16@meituan.com>
2022-05-10 13:33:50 +0800

    upadte

--- src/9.sort/9.quick.ts
+++ src/9.sort/9.quick.ts
@@ -3,15 +3,19 @@
  * Date: May 9, 2022
  *
  * Quick Sort
- *
- *
- *
+ * 
+ * - Find array pivot, put smaller values to its left and bigger values to its right
+ * - Process left and right arrays recursively
+ * 
+ * - In practice, we firstly put pivot to leftest
+ * - Traverse array, make smallers left and biggers right
+ * - Swap pivot and rightest of smallers
  */
 
 import { init, swap } from '../common/utils';
 
 const quickSort = (arr: number[]): void => {
-  
+
 };
 
 const arr = init();
