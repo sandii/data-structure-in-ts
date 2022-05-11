fc42951cda983c88a7007355dfeb58b52c4c9557
chenzhi16 <chenzhi16@meituan.com>
2022-05-11 10:39:53 +0800

    update

--- src/9.sort/4.shell.ts
+++ src/9.sort/4.shell.ts
@@ -4,10 +4,10 @@
  *
  * Shell Sort
  *
- * - Invented in 1959, breaking O(n^2) limit firstly.
- * - Upgraded verson of Insert Sort, avoiding move some elements for long distance.
+ * - Invented in 1959, breaking O(n^2) limit historically.
+ * - Upgraded verson of Insert Sort, avoiding to move some elements for long distance.
  *
- * - In practice, firstly sort cards in some gap, then gradually shrink gap, until gap becomes 1.
+ * - In practice, firstly sort elements in some gap, then gradually shrink gap, until gap becomes 1.
  * - Dr. Knuth suggests gap = gap / 3 + 1, reason is too complicated for common people to understand.
  *
  * - Set gap to 1, you'll find it is no more than Insert Sort.
@@ -16,8 +16,26 @@
 
 import { init, swap } from '../common/utils';
 
+const updateGap = (gap: number): number => {
+  return gap <= 1 ? 0 : Math.floor(gap / 3 + 1);
+};
+
 const shellSort = (arr: number[]): void => {
-  // todo
+  for (
+    let gap = updateGap(arr.length);
+    gap > 0;
+    gap = updateGap(gap)
+  ) {
+    for (let i = gap; i < arr.length; i++) {
+      for (let j = i - gap; j >= 0; j--) {
+        if (arr[j + gap] < arr[j]) {
+          swap(arr, j + gap, j);
+        } else {
+          break;
+        }
+      }
+    }
+  }
 };
 
 const arr = init();
