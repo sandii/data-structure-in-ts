b8434e13a6b79f1cd2a46f3d0f8ab978f733b25c
chenzhi16 <chenzhi16@meituan.com>
2022-09-01 10:49:59 +0800

    update

---
+++ src/9.sort/6-1.merge.ts
@@ -0,0 +1,55 @@
+/*
+ * Author: Chen Zhi <chenzhibupt@qq.com>
+ * Date: Sep 9, 2022
+ *
+ * Merge Sort
+ *
+ * Recursively split array into 2, until the sub-array only contains 1 element.
+ * Merge 2 sorted array back together. 
+ *
+ */
+
+import { init, swap } from '../common/utils';
+
+const merge = (
+  left: number[],
+  right: number[],
+): number[] => {
+  let result: number[] = [];
+
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      result.push(left.shift() as number);
+    } else {
+      result.push(right.shift() as number);
+    }
+  }
+
+  if (left.length) {
+    result = [...result, ...left];
+  }
+  if (right.length) {
+    result = [...result, ...right];
+  }
+
+  return result;
+};
+
+const mergeSort = (arr: number[]): number[] => {
+  if (arr.length < 2) return arr;
+
+  const mid = Math.floor(arr.length / 2);
+  let left = arr.slice(0, mid);
+  let right = arr.slice(mid);
+
+  left = mergeSort(left);
+  right = mergeSort(right);
+  const result = merge(left, right);
+
+  return result;
+};
+
+let arr = init(40);
+console.log(arr);
+arr = mergeSort(arr);
+console.log(arr);
---
+++ src/9.sort/6-2.merge-non-recursive.ts
@@ -0,0 +1,10 @@
+/*
+ * Author: Chen Zhi <chenzhibupt@qq.com>
+ * Date: Sep 9, 2022
+ *
+ * Merge Sort
+ *
+ *
+ *
+ *
+ */
