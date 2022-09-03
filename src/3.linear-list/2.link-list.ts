c9dfa9d55aec5d633d377afb65447b2b5f6f09af
chenzhi16 <chenzhi16@meituan.com>
2022-09-03 11:56:40 +0800

    update

--- src/3.linear-list/2.link-list.ts
+++ src/3.linear-list/2.link-list.ts
@@ -3,12 +3,12 @@
  * Date: Sep 3, 2022
  *
  * Link List
- *
+ * - index from 1
  *
  *
  */
 
-import { ElType } from '../type';
+import { ElType, Status } from '../type';
 
 type Pointer = LinkListNode | null;
 
@@ -21,4 +21,31 @@ class LinkListNode {
 
 class LinkList {
   public head: Pointer = null;
+
+  public insert(el: ElType, index = 1): Status {
+    if (index < 1) return Status.FAIL;
+
+    if (index === 1) {
+      this.insertAtHead(el);
+      return Status.SUCCESS;
+    }
+
+    let previous = this.head as LinkListNode;
+    for (let j = 1; j < index; j++) {
+      if (previous.next === null) break; // index too large, insert at tail
+      previous = previous.next;
+    }
+    previous.next = new LinkListNode(el);
+
+    return Status.SUCCESS;
+  }
+
+  // public insertAtHead(el: ElType): void {}
+  // public insertAtTail(el: ElType): void {}
+  // public remove(index: number): void {}
+  // public getElem(index: number): ElType {}
+  // public locateElem(el: ElType): number {}
+  // public getLength(): number {}
+  // public clear() {}
+  // public traverse() {}
 }
