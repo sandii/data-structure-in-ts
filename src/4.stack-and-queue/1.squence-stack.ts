34220611a0206a8bf8a19070890decc8b3f4a89b
chenzhi16 <chenzhi16@meituan.com>
2022-09-07 22:23:36 +0800

    update

--- src/4.stack-and-queue/1.squence-stack.ts
+++ src/4.stack-and-queue/1.squence-stack.ts
@@ -1,7 +1,25 @@
 /*
-* author: Chenzhi <chenzhibupt@qq.com>
-* date: Sep 6, 2022
-*
-* Sequence Stack
-* - 
-*/
+ * author: Chenzhi <chenzhibupt@qq.com>
+ * date: Sep 6, 2022
+ *
+ * Sequence Stack
+ * - Top is -1 when stack is empty
+ */
+
+import { ElType, Status } from '../type';
+
+class SequenceStack {
+  public constructor(private MAXSIZE = 20) {}
+
+  private list: ElType[] = [];
+  private top = -1;
+
+  public isEmpty(): boolean {
+    return this.top === -1;
+  }
+  public isFull(): boolean {
+    return this.top === this.MAXSIZE - 1;
+  }
+  public push(n: ElType): Status {}
+  public pop(): ElType {}
+}
---
+++ src/4.stack-and-queue/2.link-stack.ts
@@ -0,0 +1,10 @@
+/*
+* author: Chenzhi <chenzhibupt@qq.com>
+* date: Sep 7, 2022
+*
+* Link Stack
+* - Always insert at head
+*/
+
+
+
