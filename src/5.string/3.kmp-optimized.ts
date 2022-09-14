9f0b349e766fefa6246fd9b1bf4cc93f4b3c1b9c
chenzhi16 <chenzhi16@meituan.com>
2022-09-14 21:30:28 +0800

    update

--- src/5.string/3.kmp-optimized.ts
+++ src/5.string/3.kmp-optimized.ts
@@ -20,19 +20,86 @@
  * next[5] = _next[5] = 3: pattern[3] !== pattern[5], move back no more
  *
  * ## Example:
- * 
+ *
  * ababcccccc
  * ||||x
  * ababacd
- * 
+ *
  * - Normal KMP: Move back to 2, match fails again
  * ababcccccc
  *   ||x
  *   ababacd
- * 
+ *
  * - Optimized KMP: Move back to 0 directily
  * ababcccccc
  *     x
  *     ababacd
- * 
+ *
  */
+
+const getNext = (p: string): number[] => {
+  const next: number[] = [];
+  next[0] = 0;
+  next[1] = 0;
+
+  let sta = 0;
+  let end = 1;
+
+  while (end + 1 < p.length) {
+    if (p[sta] === p[end]) {
+      sta++;
+    } else {
+      sta = 0;
+    }
+
+    end++;
+
+    if (p[sta] === p[end]) {
+      next[end] = next[sta];
+    } else {
+      next[end] = sta;
+    }
+  }
+
+  return next;
+};
+
+const betterKMP = (s: string, p: string): number => {
+  if (!s.length || !p.length) return -1;
+
+  const next = getNext(p);
+  let si = 0;
+  let pi = 0;
+
+  while (si < s.length && pi < p.length) {
+    if (s[si] === p[pi]) {
+      si++;
+      pi++;
+      continue;
+    }
+
+    if (pi === 0) {
+      si++;
+    } else {
+      pi = next[pi];
+    }
+  }
+
+  if (pi === p.length) {
+    return si - pi;
+  } else {
+    return -1;
+  }
+};
+
+console.log(getNext('ababacd')); // 0 0 0 0 0 3 0
+
+console.log(betterKMP('hello', 'hel')); // 0
+console.log(betterKMP('hello', 'ell')); // 1
+console.log(betterKMP('hello', 'loo')); // -1
+console.log(betterKMP('hello', 'll')); // 2
+console.log(betterKMP('hello', '')); // -1
+console.log(betterKMP('hello', 'asdf')); // -1
+console.log(betterKMP('abcabx', 'abx')); // 3
+console.log(betterKMP('ababababax', 'abx')); // -1
+console.log(betterKMP('abcabcabcabc', 'abcd')); // -1
