3c1befa2195a0f3753ce759e5a3ff71cb62bfdac
chenzhi16 <chenzhi16@meituan.com>
2022-09-20 11:38:08 +0800

    update

--- src/7.graph/2.adjacency-list.ts
+++ src/7.graph/2.adjacency-list.ts
@@ -27,4 +27,45 @@ class AdjacencyList {
   private vertex: VertexNode[] = [];
   private vertexNum = 0;
   private arcNum = 0;
+
+  private visited: boolean[] = [];
+  private queue: number[] = [];
+
+  public constructor(vStr: string, aStr: string) {
+    this.initVertext(vStr);
+    this.initArc(aStr);
+  }
+
+  private initVertext(vStr: string): void {
+    this.vertexNum = vStr.length;
+    this.vertex = vStr
+      .split('')
+      .map(s => new VertexNode(s));
+  }
+
+  private initArc(aStr: string): void {
+    const arr = aStr.split(',');
+    this.arcNum = arr.length;
+
+    arr.forEach(str => {
+      const [i, j, weight] = str.split('-');
+      const vertexItem = this.vertex[Number(i)];
+      if (!vertexItem) return;
+
+      const arc = new ArcNode();
+      arc.adjacency = Number(j);
+      arc.weight = Number(weight);
+      arc.next = vertexItem.firstArc;
+
+      vertexItem.firstArc = arc;
+    });
+  }
+
+  public traverseDFS(): void {}
+  
+  private visitDFS(): void {}
+
+  public traverseBFS(): void {}
+  
+  private visitBFS(): void {}
 }
