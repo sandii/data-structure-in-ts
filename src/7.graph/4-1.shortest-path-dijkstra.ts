c55afcfa52c5391ae5a2c043083c35c1fc0b31f6
chenzhi16 <chenzhi16@meituan.com>
2022-09-23 01:12:27 +0800

    update

--- src/7.graph/4-1.shortest-path-dijkstra.ts
+++ src/7.graph/4-1.shortest-path-dijkstra.ts
@@ -20,7 +20,7 @@ import AdjacencyMatrix from './common/1.adjacency-matrix';
 
 class ActiveVertex {
   public constructor(
-    public data = '',
+    public data = '0',
     public prev = 0,
     public cost = 0,
   ) {}
@@ -60,35 +60,36 @@ class MyAdjacencyMatrix extends AdjacencyMatrix {
   private visitDijkstra(i: number): void {
     this.addNewBatch(i);
     const j = this.getNearestVertex();
+    if (this.visited[j]!.done) return;
     this.visited[j]!.done = true;
     this.visitDijkstra(j);
   }
 
-  private addNewBatch(i: number): void {
-    for (let j = 0; j < this.vertexNum; j++) {
+  private addNewBatch (i: number): void {
+    for (let j = 1; j < this.vertexNum; j++) {
       const weight = this.arc[i][j];
       if (this.visited[j]?.done) continue;
       if (weight === 0 || weight === Infinity) continue;
 
       const prevCost = this.visited[i]?.cost || 0;
       const currCost = prevCost + weight;
-      const visitedCost = this.visited[j]?.cost || 0;
-      if (currCost >= visitedCost) continue;
-
-      this.visited[j] = new ActiveVertex(
-        this.vertex[j],
-        i,
-        currCost,
-      );
+      const visitedCost = this.visited[j]?.cost || Infinity;
+      if (currCost < visitedCost) {
+        this.visited[j] = new ActiveVertex(
+          this.vertex[j],
+          i,
+          currCost,
+        );
+      }
     }
   }
 
   private getNearestVertex(): number {
     let minCost = Infinity;
     let minIndex = 0;
-    for (let j = 0; j < this.vertexNum; j++) {
+    for (let j = 1; j < this.vertexNum; j++) {
       const vertex = this.visited[j];
-      if (!vertex) continue;
+      if (!vertex || vertex.done) continue;
 
       if (vertex.cost < minCost) {
         minCost = vertex.cost;
@@ -100,8 +101,8 @@ class MyAdjacencyMatrix extends AdjacencyMatrix {
 }
 
 const matrix = new MyAdjacencyMatrix(
-  '12345678',
+  '012345678',
   '0-1-1,0-2-5,1-0-1,1-2-3,1-3-7,1-4-5,2-0-5,2-1-3,2-4-1,2-5-7,3-1-7,3-4-2,3-6-3,4-1-5,4-2-1,4-3-2,4-5-3,4-6-6,4-7-9,5-2-7,5-4-3,5-7-5,6-3-3,6-4-6,6-7-2,6-8-7,7-4-9,7-5-5,7-6-2,7-8-4,8-6-7,8-7-4',
 );
 matrix.dijkstra();
-matrix.printPath();
+matrix.printPath(); // 01243678
