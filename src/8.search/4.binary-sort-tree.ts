a752383dde01ec2650753100fb8b8eabf018112a
chenzhi16 <chenzhi16@meituan.com>
2022-09-28 10:25:58 +0800

    update

--- src/8.search/4.binary-sort-tree.ts
+++ src/8.search/4.binary-sort-tree.ts
@@ -5,7 +5,7 @@
  * Binary Sort Tree
  *
  * If lchild and rchild both exit,
- * Find rightest (biggest) offspring of lchild to replace removed node
+ * Find rightest (max) offspring of lchild to replace removed node
  * Because rightest offspring of lchild is still smaller than rchild
  *
  */
@@ -19,98 +19,103 @@ class TreeNode {
 class BinarySortTree {
   private root: TreeNode | null = null;
 
-  private parent: TreeNode | null = null;
-
-  public search(el: number): boolean {
-    this.parent = null; // init
-    return this.doSearch(el, this.root);
+  public search(data: number): boolean {
+    return this.doSearch(this.root, data);
   }
 
   private doSearch(
-    el: number,
     curr: TreeNode | null,
+    data: number,
   ): boolean {
     if (!curr) return false;
-    if (el === curr.data) return true;
 
-    this.parent = curr;
-    if (el < curr.data) {
-      return this.doSearch(el, curr.lchild);
+    if (data < curr.data) {
+      return this.doSearch(curr.lchild, data);
     } else {
-      return this.doSearch(el, curr.rchild);
+      return this.doSearch(curr.rchild, data);
     }
   }
 
-  public insert(el: number): void {
+  public insert(data: number): void {
+    const newNode = new TreeNode(data);
     if (!this.root) {
-      this.root = new TreeNode(el);
+      this.root = newNode;
       return;
     }
+    this.doInsert(this.root, newNode);
+  }
 
-    const isExist = this.search(el);
-    if (isExist) return;
+  private doInsert(parent: TreeNode, newNode: TreeNode) {
+    if (newNode.data === parent.data) return;
 
-    const newNode = new TreeNode(el);
-    if (el < this.parent!.data) {
-      this.parent!.lchild = newNode;
+    if (newNode.data < parent.data) {
+      if (!parent.lchild) {
+        parent.lchild = newNode;
+      } else {
+        this.doInsert(parent.lchild, newNode);
+      }
     } else {
-      this.parent!.rchild = newNode;
+      if (!parent.rchild) {
+        parent.rchild = newNode;
+      } else {
+        this.doInsert(parent.rchild, newNode);
+      }
     }
   }
 
-  public remove(el: number): void {
-    if (!this.root) return;
-
-    const isExist = this.search(el);
-    if (!isExist) return;
-
-    const currSide =
-      el < this.parent!.data ? 'lchild' : 'rchild';
-
-    // node to be removed
-    const curr = this.parent![currSide]!;
-
-    if (!curr.lchild || !curr.rchild) {
-      // one child or no child
-      this.parent![currSide] = curr.lchild || curr.rchild;
-    } else {
-      // two children
-      this.handleTwoChildren(curr, currSide);
-    }
+  public remove(data: number): void {
+    this.root = this.doRemove(this.root, data);
   }
 
-  private handleTwoChildren(
-    curr: TreeNode,
-    currSide: 'lchild' | 'rchild',
-  ): void {
-    // use lchild's rightest offspring to replace current node
+  private doRemove(
+    curr: TreeNode | null,
+    data: number,
+  ): TreeNode | null {
+    if (!curr) return null;
 
-    // if lchild has no rchild
-    if (!curr.lchild?.rchild) {
-      this.parent![currSide]= curr.lchild;
-      return;
+    if (data < curr.data) {
+      curr.lchild = this.doRemove(curr.lchild, data);
+      return curr;
+    }
+    if (data > curr.data) {
+      curr.rchild = this.doRemove(curr.rchild, data);
+      return curr;
+    }
+    // delete current node
+    // current node has no child
+    if (!curr.lchild && !curr.rchild) {
+      return null;
+    }
+    // current node has lchild
+    if (curr.lchild && !curr.rchild) {
+      return curr.lchild;
+    }
+    // current node has rchild
+    if (!curr.lchild && curr.rchild) {
+      return curr.rchild;
     }
 
-    let rightestOffspring = curr.lchild!;
-    let rightestOffspringParent = curr;
-    while (rightestOffspring.rchild) {
-      rightestOffspringParent = rightestOffspring;
-      rightestOffspring = rightestOffspring.rchild;
-    }
-
-    curr.data = rightestOffspring.data;
-    rightestOffspringParent.rchild = rightestOffspring.lchild;
+    // current node has both children
+    // replace current node with lchild's rightest offspring
+    const leftMaxNode = this.getMaxNode(curr.lchild!);
+    curr.data = leftMaxNode.data;
+    curr.lchild = this.doRemove(curr.lchild, leftMaxNode.data);
+    return curr;
   }
 
-  public traverse(): void {
-    this.doTraverse(this.root);
+  public getMaxNode(curr: TreeNode): TreeNode {
+    
   }
 
-  private doTraverse(curr: TreeNode | null): void {
+  public inOrdertraverse(): void {
+    this.doInOrderTraverse(this.root);
+  }
+
+  private doInOrderTraverse(curr: TreeNode | null): void {
     if (!curr) return;
-    this.doTraverse(curr.lchild);
+    this.doInOrderTraverse(curr.lchild);
     console.log(curr.data);
-    this.doTraverse(curr.rchild);
+    this.doInOrderTraverse(curr.rchild);
   }
 }
 
@@ -118,21 +123,21 @@ export default BinarySortTree;
 
 const tree = new BinarySortTree();
 
-for (let i = 0; i < 20; i++) {
-  const el = Math.floor(Math.random() * 100);
-  console.log(el);
-  tree.insert(el);
-}
+// for (let i = 0; i < 20; i++) {
+//   const el = Math.floor(Math.random() * 100);
+//   console.log(el);
+//   tree.insert(el);
+// }
 
-console.log('\nSorted...\n');
+// console.log('\nSorted...\n');
 
-tree.traverse();
+// tree.traverse();
 
-console.log('\nRemoved all muliples of 5...\n');
+// console.log('\nRemoved all muliples of 5...\n');
 
-for (let i = 0; i < 20; i++) {
-  const el = (i + 1) * 5;
-  tree.remove(el);
-}
+// for (let i = 0; i < 20; i++) {
+//   const el = (i + 1) * 5;
+//   tree.remove(el);
+// }
 
-tree.traverse();
+// tree.traverse();
