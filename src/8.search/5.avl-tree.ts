2bd3fb766da29361627874b8142a34ca5ae53612
chenzhi16 <chenzhi16@meituan.com>
2022-10-08 12:32:15 +0800

    update

--- src/8.search/5.avl-tree.ts
+++ src/8.search/5.avl-tree.ts
@@ -10,6 +10,12 @@
  *
  */
 
+enum BalanceFactor {
+  LEFT_TALLER = 1,
+  EQUAL_TALLER = 0,
+  RIGHT_TALLER = -1,
+}
+
 class TreeNode {
   public constructor(public data = 0) {}
   public lchild: TreeNode | null = null;
@@ -18,8 +24,66 @@ class TreeNode {
 
 class AVLTree {
   private root: TreeNode | null = null;
+  private taller = false;
 
-  public insert(data: number): void {}
+  public insert(data: number): void {
+    this.root = this.doInsert(this.root, data);
+  }
+
+  private doInsert(
+    node: TreeNode | null,
+    data: number,
+  ): TreeNode {
+    const newNode = new TreeNode(data);
+
+    if (!node) {
+      this.taller = true;
+      return newNode;
+    }
+
+    if (data === node.data) {
+      this.taller = false;
+      return node;
+    }
+
+    const bf = this.getBalanceFactor(node);
+
+    if (data < node.data) {
+      node.lchild = this.doInsert(node.lchild, data);
+      if (!this.taller) return node;
+
+      switch (bf) {
+        case BalanceFactor.LEFT_TALLER:
+          this.taller = false;
+          return this.balanceL(node);
+        case BalanceFactor.EQUAL_TALLER:
+          this.taller = true;
+          return node;
+        case BalanceFactor.RIGHT_TALLER:
+          this.taller = false;
+          return node;
+      }
+    }
+
+    if (data > node.data) {
+      node.rchild = this.doInsert(node.rchild, data);
+      if (!this.taller) return node;
+
+      switch (bf) {
+        case BalanceFactor.RIGHT_TALLER:
+          this.taller = false;
+          return this.balanceR(node);
+        case BalanceFactor.EQUAL_TALLER:
+          this.taller = true;
+          return node;
+        case BalanceFactor.LEFT_TALLER:
+          this.taller = false;
+          return node;
+      }
+    }
+
+    return node;
+  }
 
   private rotateR(oldTop: TreeNode): TreeNode {
     const newTop = oldTop.lchild!;
@@ -37,7 +101,7 @@ class AVLTree {
 
   private balanceR(node: TreeNode): TreeNode {
     const bf = this.getBalanceFactor(node.rchild);
-    if (bf < 0) {
+    if (bf === BalanceFactor.RIGHT_TALLER) {
       return this.rotateL(node);
     } else {
       node.rchild = this.rotateR(node.rchild!);
@@ -47,7 +111,7 @@ class AVLTree {
 
   private balanceL(node: TreeNode): TreeNode {
     const bf = this.getBalanceFactor(node.lchild);
-    if (bf > 0) {
+    if (bf === BalanceFactor.LEFT_TALLER) {
       return this.rotateR(node);
     } else {
       node.lchild = this.rotateL(node.lchild!);
@@ -85,11 +149,18 @@ class AVLTree {
   // bf > 0: left is taller
   // bf < 0: right is taller
   private getBalanceFactor(node: TreeNode | null): number {
-    if (!node) return 0;
-    return (
+    if (!node) return BalanceFactor.EQUAL_TALLER;
+    const bf =
       this.getBalanceFactor(node.lchild) -
-      this.getBalanceFactor(node.rchild)
-    );
+      this.getBalanceFactor(node.rchild);
+
+    if (bf === 0) return BalanceFactor.EQUAL_TALLER;
+
+    if (bf > 0) {
+      return BalanceFactor.LEFT_TALLER;
+    } else {
+      return BalanceFactor.RIGHT_TALLER;
+    }
   }
 }
 
