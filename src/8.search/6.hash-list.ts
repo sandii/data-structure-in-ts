f1ff665a15b5789343c5a7f034d84b7f5cb39456
chenzhi16 <chenzhi16@meituan.com>
2022-10-10 17:28:31 +0800

    update

--- src/8.search/6.hash-list.ts
+++ src/8.search/6.hash-list.ts
@@ -5,7 +5,7 @@
  * Hash List
  *
  * There are many methods to generate hash, modulus was used here
- * 
+ *
  * We use an array to store data
  * Array index is key (modulus)
  * Link-list on every cell to to solve collision problem
@@ -31,20 +31,61 @@ class HashList {
     for (let i = 0; i < this.MAXSIZE; i++) {
       this.list[i] = new Cell();
     }
-    
   }
 
-  private hash(): number {
-    return 0;
+  private hash(data: number): number {
+    return Math.abs(data % this.MAXSIZE);
   }
 
-  private insert(data: number): void {}
+  public insert(data: number): void {
+    const hash = this.hash(data);
+    let prev = this.list[hash];
+    while (prev.next) {
+      if (prev.data === data) return; // already exists
+      prev = prev.next;
+    }
+    prev.next = new Cell(data);
+  }
 
-  private remove(data: number): void {}
+  public remove(data: number): void {
+    const hash = this.hash(data);
+    let prev = this.list[hash];
+    let curr = prev.next;
+    while (curr) {
+      if (curr.data === data) break;
+      prev = curr;
+      curr = curr.next;
+    }
+    if (!curr) return; // not exist
+    prev.next = curr.next;
+  }
 
-  private isExist(data: number): boolean {
+  public isExist(data: number): boolean {
+    const hash = this.hash(data);
+    let curr = this.list[hash].next;
+    while (curr) {
+      if (curr.data === data) return true;
+      curr = curr.next;
+    }
     return false;
   }
 }
 
 export default HashList;
+
+const MAXSIZE = 12;
+const list = new HashList(MAXSIZE);
+
+for (let i = 0; i < MAXSIZE; i++) {
+  list.insert(i * 5);
+}
+
+console.log(`Is 5 exists? ${list.isExist(5)}`);
+console.log(`Is 20 exists? ${list.isExist(20)}`);
+console.log(`Is 14 exists? ${list.isExist(14)}`);
+console.log(`Is -14 exists? ${list.isExist(-14)}`);
+console.log(`Is 55 exists? ${list.isExist(55)}`);
+
+console.log('Remove 55.');
+list.remove(55);
+console.log(`Is 55 exists? ${list.isExist(55)}`);
